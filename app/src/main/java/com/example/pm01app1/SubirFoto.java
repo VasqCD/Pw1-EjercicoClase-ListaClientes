25fb0a930daa7bb8971e234b8b35eb3502bc67f6
Christian Vasquez <128773362+VasqCD@users.noreply.github.com>
2025-02-13 23:02:22 -0600

    Implement photo selection and capture functionality in SubirFoto activity

--- app/src/main/java/com/example/pm01app1/SubirFoto.java
+++ app/src/main/java/com/example/pm01app1/SubirFoto.java
@@ -1,98 +1,99 @@
 package com.example.pm01app1;
 
-            import android.content.DialogInterface;
-            import android.content.Intent;
-            import android.graphics.Bitmap;
-            import android.net.Uri;
-            import android.os.Bundle;
-            import android.provider.MediaStore;
-            import android.widget.ImageButton;
-            import android.widget.ImageView;
-            import android.widget.Toast;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-            import androidx.activity.EdgeToEdge;
-            import androidx.annotation.Nullable;
-            import androidx.appcompat.app.AlertDialog;
-            import androidx.appcompat.app.AppCompatActivity;
-            import androidx.core.graphics.Insets;
-            import androidx.core.view.ViewCompat;
-            import androidx.core.view.WindowInsetsCompat;
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
-            public class SubirFoto extends AppCompatActivity {
+public class SubirFoto extends AppCompatActivity {
 
-                private static final int REQUEST_IMAGE_CAPTURE = 1;
-                private static final int REQUEST_IMAGE_PICK = 2;
-                private ImageView fotografiaTomada;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_IMAGE_PICK = 2;
+    private ImageView fotografiaTomada;
 
-                @Override
-                protected void onCreate(Bundle savedInstanceState) {
-                    super.onCreate(savedInstanceState);
-                    EdgeToEdge.enable(this);
-                    setContentView(R.layout.activity_subir_foto);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_subir_foto);
 
-                    fotografiaTomada = findViewById(R.id.fotografiaTomada);
-                    ImageButton tomarFoto = findViewById(R.id.tomarFoto);
+        fotografiaTomada = findViewById(R.id.fotografiaTomada);
+        ImageButton tomarFoto = findViewById(R.id.tomarFoto);
 
-                    tomarFoto.setOnClickListener(v -> {
-                        showPictureDialog();
-                    });
+        tomarFoto.setOnClickListener(v -> {
+            showPictureDialog();
+        });
 
-                    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-                        Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-                        v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-                        return insets;
-                    });
-                }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
 
-                private void showPictureDialog() {
-                    AlertDialog.Builder pictureDialog = new AlertDialog.Builder(this);
-                    pictureDialog.setTitle("Seleccione una opcion");
-                    String[] pictureDialogItems = {
-                            "Seleccionar foto de galeria",
-                            "Tomar Fotografia" };
-                    pictureDialog.setItems(pictureDialogItems,
-                            (dialog, which) -> {
-                                switch (which) {
-                                    case 0:
-                                        choosePhotoFromGallery();
-                                        break;
-                                    case 1:
-                                        takePhotoFromCamera();
-                                        break;
-                                }
-                            });
-                    pictureDialog.show();
-                }
-
-                public void choosePhotoFromGallery() {
-                    Intent galleryIntent = new Intent(Intent.ACTION_PICK,
-                            android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                    startActivityForResult(galleryIntent, REQUEST_IMAGE_PICK);
-                }
-
-                private void takePhotoFromCamera() {
-                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+    private void showPictureDialog() {
+        AlertDialog.Builder pictureDialog = new AlertDialog.Builder(this);
+        pictureDialog.setTitle("Seleccione una opción");
+        String[] pictureDialogItems = {
+                "Seleccionar foto de la galería",
+                "Tomar usando la camara" };
+        pictureDialog.setItems(pictureDialogItems,
+                (dialog, which) -> {
+                    switch (which) {
+                        case 0:
+                            choosePhotoFromGallery();
+                            break;
+                        case 1:
+                            takePhotoFromCamera();
+                            break;
                     }
-                }
+                });
+        pictureDialog.show();
+    }
 
-                @Override
-                protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-                    super.onActivityResult(requestCode, resultCode, data);
-                    if (resultCode == RESULT_OK) {
-                        if (requestCode == REQUEST_IMAGE_CAPTURE) {
-                            Bundle extras = data.getExtras();
-                            Bitmap imageBitmap = (Bitmap) extras.get("data");
-                            fotografiaTomada.setImageBitmap(imageBitmap);
-                        } else if (requestCode == REQUEST_IMAGE_PICK) {
-                            Uri selectedImageUri = data.getData();
-                            if (selectedImageUri != null) {
-                                fotografiaTomada.setImageURI(selectedImageUri);
-                            }
-                        }
-                    } else {
-                        Toast.makeText(this, "You haven't picked Image", Toast.LENGTH_SHORT).show();
-                    }
+    public void choosePhotoFromGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, REQUEST_IMAGE_PICK);
+    }
+
+    private void takePhotoFromCamera() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    // este metodo se ejecuta despues de tomar
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_IMAGE_CAPTURE) {
+                Bundle extras = data.getExtras();
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                fotografiaTomada.setImageBitmap(imageBitmap);
+            } else if (requestCode == REQUEST_IMAGE_PICK) {
+                Uri selectedImageUri = data.getData();
+                if (selectedImageUri != null) {
+                    fotografiaTomada.setImageURI(selectedImageUri);
                 }
             }
+        } else {
+            Toast.makeText(this, "No se tomo la fotografia", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
